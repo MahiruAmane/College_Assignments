5ab5211e65f72b3cf7644d5f30eefc8134f7056f
Shiina Mahiru <piyush124790@gmail.com>
2025-12-18 14:11:06 +0530

    Question 2 Of Shell Scripting
    Write a Bash Script To Take User Input And Display A Welcome Message.

---
+++ Linux Shell Scripting (All Files)/Experiment Report (Bash)/Question_2.sh
@@ -0,0 +1,8 @@
+# Write a Bash Script To Take User Input And Display A Welcome Message.
+
+#!/bin/bash
+
+echo -n "Please Enter Your Name : "
+read name
+
+echo "Hello, $name! Welcome to Shell Scripting."
