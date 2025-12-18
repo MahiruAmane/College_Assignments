8e2f62486de9ccae7df8e44ab1904462fbcef5f6
Shiina Mahiru <piyush124790@gmail.com>
2025-12-18 14:05:25 +0530

    Updating Repository
    Addition Of New Content (Linux Shell Scripting).

--- Shell Scripting (All Files)/Experiment Report (Bash)/Question_2.sh
+++
@@ -1,8 +0,0 @@
-# Write a Bash Script To Take User Input And Display A Welcome Message.
-
-#!/bin/bash
-
-echo -n "Please Enter Your Name : "
-read name
-
-echo "Hello, $name! Welcome to Shell Scripting."
