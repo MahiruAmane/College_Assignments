60d0b74ff66f1639bda87e30544fcf1f7484d220
Stella Hoshino <piyush124790@gmail.com>
2026-02-02 01:13:46 +0530

    Question 4 Of Python Programming
    Write a Python Program To Perform Basic Arithmetic Operations On Two Numbers.

---
+++ Python Programming (All Files)/Experiment 1 (Basics)/Question_04.py
@@ -0,0 +1,16 @@
+# Write a Python Program To Perform Basic Arithmetic Operations On Two Numbers.
+
+a = int(input("Please Enter The First Number : "))
+b = int(input("Please Enter The Second Number : "))
+
+result = a + b
+print("The Addition Of {} & {} Is : ".format(a, b), result)
+
+result = a - b
+print("The Subtraction Of {} & {} Is : ".format(a, b), result)
+
+result = a * b
+print("The Multiplication Of {} & {} Is : ".format(a, b), result)
+
+result = a / b
+print("The Division Of {} & {} Is : ".format(a, b), result)
