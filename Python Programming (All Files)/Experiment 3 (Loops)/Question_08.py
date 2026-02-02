4fb59f8c07b3b19d05c078b655d19e634a76ac82
Stella Hoshino <piyush124790@gmail.com>
2026-02-02 21:00:53 +0530

    Question 8 Of Python Programming
    Write a Python Program To Convert All Lower Cases to Upper Case In a String.

---
+++ Python Programming (All Files)/Experiment 3 (Loops)/Question_08.py
@@ -0,0 +1,5 @@
+# Write a Python Program To Convert All Lower Cases to Upper Case In a String.
+
+string = input("Please Enter a String : ")
+upper_string = string.upper()
+print("String in Upper Case : ", upper_string)
