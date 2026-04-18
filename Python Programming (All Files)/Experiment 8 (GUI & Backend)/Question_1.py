0e691cde986431ab593ad368f0e80ef0152f1af2
Mahiru Amane <piyush124790@gmail.com>
2026-04-18 13:48:26 +0530

    Question 1 Of Python Programming
    Write a Python Program To Create a Simple Tkinter Window With a Title & Fixed Size.

---
+++ Python Programming (All Files)/Experiment 8 (GUI & Backend)/Question_1.py
@@ -0,0 +1,8 @@
+# Write a Python Program To Create a Simple Tkinter Window With a Title & Fixed Size.
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Simple Tkinter Window")
+root.geometry("1280x720")
+root.mainloop()
